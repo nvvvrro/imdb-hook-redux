37514a6755fe03670da8ba205a726046a60e2ef8
nvvvrro <m.navarrocarrillo@gmail.com>
2021-11-09 19:25:24 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Typography } from "@mui/material";
+
+const App = () => {
+  return (
+    <Container>
+      <Typography variant="inherit">Hello World</Typography>
+    </Container>
+  );
+};
+
+export default App;
